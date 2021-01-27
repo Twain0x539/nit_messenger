04f57ad593ea4d1c0c2f5e7b735bbfd90e3aa223
Mihail Karaev <karaevmihail@gmail.com>
2021-01-28 02:18:35 +0500

    routes.py contains future endpoints

--- transport/sanic/endpoints/__init__.py
+++ transport/sanic/endpoints/__init__.py
@@ -1,2 +1,6 @@
 from .base import BaseEndpoint
 from .health import HealthEndpoint
+
+from .message.create import CreateMessageEndpoint
+from .message.edit import EditMessageEndpoint
+from .message.delete import DeleteMessageEndpoint
--- transport/sanic/routes.py
+++ transport/sanic/routes.py
@@ -10,4 +10,20 @@ def get_routes(config: ApplicationConfig, context: Context) -> Tuple:
         endpoints.HealthEndpoint(
             config=config, context=context, uri='/', methods=('GET', 'POST'),
         ),
+        endpoints.CreateUserEndpoint(
+            config=config, context=context, uri='/user', methods=('POST',),
+        ),
+        endpoints.AuthUserEndpoint(
+            config=config, context=context, uri='/auth', methods=('POST',),
+        ),
+        endpoints.UserEndpoint(
+            config=config, context=context, uri='/user/<uid:int>', methods=('GET', 'POST'),
+        ),
+        endpoints.MessageEndpoint(
+            config=config, context=context, uri='/msg', methods=('GET', 'POST'),
+        ),
+        endpoints.IdentifiedMessageEndpoint(
+            config=config, context=context, uri='/msg/<uid:int>', methods=('GET', 'DELETE', "PATCH")
+        ),
+
     )
