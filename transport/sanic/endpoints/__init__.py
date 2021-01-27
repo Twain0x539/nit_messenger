e860b1ef9849052c96d79582b496aae783a32c10
Mihail Karaev <karaevmihail@gmail.com>
2021-01-27 20:42:10 +0500

    Clear architecture skeleton copied

---
+++ transport/sanic/endpoints/__init__.py
@@ -0,0 +1,2 @@
+from .base import BaseEndpoint
+from .health import HealthEndpoint
---
+++ transport/sanic/routes.py
@@ -0,0 +1,13 @@
+from typing import Tuple
+
+from configs.config import ApplicationConfig
+from context import Context
+from transport.sanic import endpoints
+
+
+def get_routes(config: ApplicationConfig, context: Context) -> Tuple:
+    return (
+        endpoints.HealthEndpoint(
+            config=config, context=context, uri='/', methods=('GET', 'POST'),
+        ),
+    )
